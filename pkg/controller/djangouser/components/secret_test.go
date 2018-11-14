6b5d7548e02cd0568cd8754a3b5331a3e4f3224a
Noah Kantrowitz <noah@coderanger.net>
2018-11-14 02:28:52 -0800

    More tests.

--- pkg/controller/djangouser/components/secret_test.go
+++ pkg/controller/djangouser/components/secret_test.go
@@ -55,4 +55,59 @@ var _ = Describe("DjangoUser Secret Component", func() {
 		Expect(ok).To(BeTrue())
 		Expect(len(password)).To(Equal(22))
 	})
+
+	It("creates a password if the secret exists but has no password key", func() {
+		instance := &summonv1beta1.DjangoUser{
+			ObjectMeta: metav1.ObjectMeta{Name: "foo.example.com", Namespace: "default"},
+			Spec: summonv1beta1.DjangoUserSpec{
+				PasswordSecret: "foo-credentials",
+				Database: &summonv1beta1.DatabaseConnection{
+					PasswordSecretRef: &summonv1beta1.SecretRef{},
+				},
+			},
+		}
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{Name: "foo-credentials", Namespace: "default"},
+		}
+		ctx := &components.ComponentContext{Top: instance, Client: fake.NewFakeClient(secret), Scheme: scheme.Scheme}
+
+		comp := djangousercomponents.NewSecret()
+		_, err := comp.Reconcile(ctx)
+		Expect(err).NotTo(HaveOccurred())
+		newSecret := &corev1.Secret{}
+		err = ctx.Client.Get(context.TODO(), types.NamespacedName{Name: "foo-credentials", Namespace: "default"}, newSecret)
+		Expect(err).NotTo(HaveOccurred())
+		password, ok := newSecret.Data["password"]
+		Expect(ok).To(BeTrue())
+		Expect(len(password)).To(Equal(22))
+	})
+
+	It("does not change an existing password", func() {
+		instance := &summonv1beta1.DjangoUser{
+			ObjectMeta: metav1.ObjectMeta{Name: "foo.example.com", Namespace: "default"},
+			Spec: summonv1beta1.DjangoUserSpec{
+				PasswordSecret: "foo-credentials",
+				Database: &summonv1beta1.DatabaseConnection{
+					PasswordSecretRef: &summonv1beta1.SecretRef{},
+				},
+			},
+		}
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{Name: "foo-credentials", Namespace: "default"},
+			Data: map[string][]byte{
+				"password": []byte("foo"),
+			},
+		}
+		ctx := &components.ComponentContext{Top: instance, Client: fake.NewFakeClient(secret), Scheme: scheme.Scheme}
+
+		comp := djangousercomponents.NewSecret()
+		_, err := comp.Reconcile(ctx)
+		Expect(err).NotTo(HaveOccurred())
+		newSecret := &corev1.Secret{}
+		err = ctx.Client.Get(context.TODO(), types.NamespacedName{Name: "foo-credentials", Namespace: "default"}, newSecret)
+		Expect(err).NotTo(HaveOccurred())
+		password, ok := newSecret.Data["password"]
+		Expect(ok).To(BeTrue())
+		Expect(password).To(Equal([]byte("foo")))
+	})
 })
