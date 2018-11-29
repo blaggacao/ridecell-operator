e0c729ffdd615ff3d7c4f38f346f0f0b412d8586
Noah Kantrowitz <noah@coderanger.net>
2018-11-28 20:11:46 -0800

    Tests for the migration component.

---
+++ pkg/controller/summon/components/migrations_test.go
@@ -0,0 +1,259 @@
+/*
+Copyright 2018 Ridecell, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package components_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	postgresv1 "github.com/zalando-incubator/postgres-operator/pkg/apis/acid.zalan.do/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	summonv1beta1 "github.com/Ridecell/ridecell-operator/pkg/apis/summon/v1beta1"
+	summoncomponents "github.com/Ridecell/ridecell-operator/pkg/controller/summon/components"
+)
+
+var _ = Describe("SummonPlatform Migrations Component", func() {
+	Describe(".IsReconcilable()", func() {
+		Context("with a zero status", func() {
+			It("returns false", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				ok := comp.IsReconcilable(ctx)
+				Expect(ok).To(BeFalse())
+			})
+		})
+
+		Context("with Postgres not ready", func() {
+			BeforeEach(func() {
+				instance.Status.PostgresStatus = postgresv1.ClusterStatusCreating
+			})
+
+			It("returns false", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				ok := comp.IsReconcilable(ctx)
+				Expect(ok).To(BeFalse())
+			})
+		})
+
+		Context("with Postgres ready", func() {
+			BeforeEach(func() {
+				instance.Status.PostgresStatus = postgresv1.ClusterStatusRunning
+			})
+
+			It("returns false", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				ok := comp.IsReconcilable(ctx)
+				Expect(ok).To(BeFalse())
+			})
+		})
+
+		Context("with Postgres and pull secret ready", func() {
+			BeforeEach(func() {
+				instance.Status.PostgresStatus = postgresv1.ClusterStatusRunning
+				instance.Status.PullSecretStatus = summonv1beta1.StatusReady
+			})
+
+			It("returns true", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				ok := comp.IsReconcilable(ctx)
+				Expect(ok).To(BeTrue())
+			})
+		})
+
+		Context("with migrations already applied", func() {
+			BeforeEach(func() {
+				instance.Status.PostgresStatus = postgresv1.ClusterStatusRunning
+				instance.Status.PullSecretStatus = summonv1beta1.StatusReady
+				instance.Status.MigrateVersion = "1.2.3"
+			})
+
+			It("returns false", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				ok := comp.IsReconcilable(ctx)
+				Expect(ok).To(BeFalse())
+			})
+		})
+	})
+
+	Describe(".Reconcile()", func() {
+		Context("with no migration job existing", func() {
+			It("creates a migration job", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				_, err := comp.Reconcile(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				job := &batchv1.Job{}
+				err = ctx.Client.Get(context.TODO(), types.NamespacedName{Name: "foo-migrations", Namespace: "default"}, job)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(instance.Status.MigrateVersion).To(Equal(""))
+			})
+		})
+
+		Context("with a running migration job", func() {
+			BeforeEach(func() {
+				job := &batchv1.Job{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo-migrations",
+						Namespace: "default",
+						Labels:    map[string]string{"app.kubernetes.io/version": "1.2.3"},
+					},
+				}
+				ctx.Client = fake.NewFakeClient(job)
+			})
+
+			It("still has a migration job", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				_, err := comp.Reconcile(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				job := &batchv1.Job{}
+				err = ctx.Client.Get(context.TODO(), types.NamespacedName{Name: "foo-migrations", Namespace: "default"}, job)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(instance.Status.MigrateVersion).To(Equal(""))
+			})
+		})
+
+		Context("with a successful migration job", func() {
+			BeforeEach(func() {
+				job := &batchv1.Job{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo-migrations",
+						Namespace: "default",
+						Labels:    map[string]string{"app.kubernetes.io/version": "1.2.3"},
+					},
+					Status: batchv1.JobStatus{
+						Succeeded: 1,
+					},
+				}
+				ctx.Client = fake.NewFakeClient(job)
+			})
+
+			It("deletes the migration", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				_, err := comp.Reconcile(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Pending controller-runtime #213
+				jobs := &metav1.List{}
+				err = ctx.Client.List(context.TODO(), &client.ListOptions{Raw: &metav1.ListOptions{TypeMeta: metav1.TypeMeta{APIVersion: "batch/v1", Kind: "Job"}}}, jobs)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(jobs.Items).To(BeEmpty())
+				Expect(instance.Status.MigrateVersion).To(Equal("1.2.3"))
+			})
+		})
+
+		Context("with a failed migration job", func() {
+			BeforeEach(func() {
+				job := &batchv1.Job{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo-migrations",
+						Namespace: "default",
+						Labels:    map[string]string{"app.kubernetes.io/version": "1.2.3"},
+					},
+					Status: batchv1.JobStatus{
+						Failed: 1,
+					},
+				}
+				ctx.Client = fake.NewFakeClient(job)
+			})
+
+			It("leaves the migration", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				_, err := comp.Reconcile(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				job := &batchv1.Job{}
+				err = ctx.Client.Get(context.TODO(), types.NamespacedName{Name: "foo-migrations", Namespace: "default"}, job)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(instance.Status.MigrateVersion).To(Equal(""))
+			})
+		})
+
+		Context("with a failed migration job from a previous version", func() {
+			BeforeEach(func() {
+				job := &batchv1.Job{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo-migrations",
+						Namespace: "default",
+						Labels:    map[string]string{"app.kubernetes.io/version": "1.2.2"},
+					},
+					Status: batchv1.JobStatus{
+						Failed: 1,
+					},
+				}
+				ctx.Client = fake.NewFakeClient(job)
+			})
+
+			It("deletes the migration and reques", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				resp, err := comp.Reconcile(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Pending controller-runtime #213
+				jobs := &metav1.List{}
+				err = ctx.Client.List(context.TODO(), &client.ListOptions{Raw: &metav1.ListOptions{TypeMeta: metav1.TypeMeta{APIVersion: "batch/v1", Kind: "Job"}}}, jobs)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(jobs.Items).To(BeEmpty())
+				Expect(resp.Requeue).To(BeTrue())
+				Expect(instance.Status.MigrateVersion).To(Equal(""))
+			})
+		})
+
+		Context("with a failed migration that has no version label", func() {
+			BeforeEach(func() {
+				job := &batchv1.Job{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo-migrations",
+						Namespace: "default",
+						Labels:    map[string]string{},
+					},
+					Status: batchv1.JobStatus{
+						Failed: 1,
+					},
+				}
+				ctx.Client = fake.NewFakeClient(job)
+			})
+
+			It("deletes the migration and reques", func() {
+				comp := summoncomponents.NewMigrations("migrations.yml.tpl")
+				resp, err := comp.Reconcile(ctx)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Pending controller-runtime #213
+				jobs := &metav1.List{}
+				err = ctx.Client.List(context.TODO(), &client.ListOptions{Raw: &metav1.ListOptions{TypeMeta: metav1.TypeMeta{APIVersion: "batch/v1", Kind: "Job"}}}, jobs)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(jobs.Items).To(BeEmpty())
+				Expect(resp.Requeue).To(BeTrue())
+				Expect(instance.Status.MigrateVersion).To(Equal(""))
+			})
+		})
+
+		Context("with a bad template", func() {
+			It("returns an error", func() {
+				comp := summoncomponents.NewMigrations("foo")
+				_, err := comp.Reconcile(ctx)
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+})
