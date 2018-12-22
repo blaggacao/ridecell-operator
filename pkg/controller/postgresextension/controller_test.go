4d3fed304c815a3758c4cbf528d74c39bef4d804
Noah Kantrowitz <noah@coderanger.net>
2018-12-22 00:56:58 -0800

    Add a controller functional test for postgresextension.

--- pkg/controller/postgresextension/controller_test.go
+++ pkg/controller/postgresextension/controller_test.go
@@ -17,46 +17,71 @@ limitations under the License.
 package postgresextension_test
 
 import (
-	"database/sql"
-	"fmt"
+	"context"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
+	dbv1beta1 "github.com/Ridecell/ridecell-operator/pkg/apis/db/v1beta1"
 	"github.com/Ridecell/ridecell-operator/pkg/dbpool"
 	"github.com/Ridecell/ridecell-operator/pkg/test_helpers"
+	"github.com/Ridecell/ridecell-operator/pkg/test_helpers/fake_sql"
 )
 
 const timeout = time.Second * 5
 
 var _ = Describe("PostgresExtension controller", func() {
 	var helpers *test_helpers.PerTestHelpers
-	var dbMock sqlmock.Sqlmock
-	var db *sql.DB
 
 	BeforeEach(func() {
 		helpers = testHelpers.SetupTest()
-
-		var err error
-		db, dbMock, err = sqlmock.New()
-		Expect(err).NotTo(HaveOccurred())
-		dbpool.Dbs.Store("postgres host=foo-database port=5432 dbname=summon user=summon password='secretdbpass' sslmode=require", db)
+		dbpool.Dbs.Store("postgres host=test-database port=5432 dbname=summon user=root password='secretdbpass' sslmode=require", fake_sql.Open())
 	})
 
 	AfterEach(func() {
 		helpers.TeardownTest()
-		db.Close()
-		dbpool.Dbs.Delete("postgres host=foo-database port=5432 dbname=summon user=summon password='secretdbpass' sslmode=require")
-
-		// Check for any unmet expectations.
-		err := dbMock.ExpectationsWereMet()
-		if err != nil {
-			Fail(fmt.Sprintf("there were unfulfilled database expectations: %s", err))
-		}
+		dbpool.Dbs.Delete("postgres host=test-database port=5432 dbname=summon user=root password='secretdbpass' sslmode=require")
 	})
 
 	It("runs a basic reconcile", func() {
+		c := helpers.Client
+
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{Name: "creds", Namespace: helpers.Namespace},
+			StringData: map[string]string{
+				"passwd": "secretdbpass",
+			},
+		}
+		err := c.Create(context.TODO(), secret)
+		Expect(err).ToNot(HaveOccurred())
+
+		ext := &dbv1beta1.PostgresExtension{
+			ObjectMeta: metav1.ObjectMeta{Name: "postgis", Namespace: helpers.Namespace},
+			Spec: dbv1beta1.PostgresExtensionSpec{
+				Database: dbv1beta1.PostgresConnection{
+					Host:     "test-database",
+					Username: "root",
+					Database: "summon",
+					PasswordSecretRef: dbv1beta1.SecretRef{
+						Name: "creds",
+						Key:  "passwd",
+					},
+				},
+			},
+		}
+		err = c.Create(context.TODO(), ext)
+		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(func() (string, error) {
+			err := c.Get(context.TODO(), types.NamespacedName{Name: "postgis", Namespace: helpers.Namespace}, ext)
+			if err != nil {
+				return "", err
+			}
+			return ext.Status.Status, nil
+		}, timeout).Should(Equal(dbv1beta1.StatusReady))
 	})
 })
