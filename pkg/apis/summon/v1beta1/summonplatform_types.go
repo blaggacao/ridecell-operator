d7d8f2ceff9f15fb70101339b4befddaab49ee50
dhouti <dhoutiforseti2103>
2019-01-24 16:29:22 -0800

    added BucketName to summon spec

--- pkg/apis/summon/v1beta1/summonplatform_types.go
+++ pkg/apis/summon/v1beta1/summonplatform_types.go
@@ -96,6 +96,9 @@ type SummonPlatformSpec struct {
 	// SQS queue setting
 	// +optional
 	SQSQueue string `json:"sqsQueue,omitempty"`
+	// AWS Bucket Name
+	// +optional
+	BucketName string `json:"bucketName,omitempty"`
 }
 
 // SummonPlatformStatus defines the observed state of SummonPlatform
