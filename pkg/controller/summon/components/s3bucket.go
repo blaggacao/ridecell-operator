ebf382ae98c84c0a8019bd50f570f01b9737b24e
dhouti <dhoutiforseti2103>
2019-01-24 16:29:54 -0800

    Added s3Bucket summon copmponent

---
+++ pkg/controller/summon/components/s3bucket.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2019 Ridecell, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package components
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+
+	awsv1beta1 "github.com/Ridecell/ridecell-operator/pkg/apis/aws/v1beta1"
+	summonv1beta1 "github.com/Ridecell/ridecell-operator/pkg/apis/summon/v1beta1"
+	"github.com/Ridecell/ridecell-operator/pkg/components"
+)
+
+type s3BucketComponent struct {
+	templatePath string
+}
+
+func NewS3Bucket(templatePath string) *s3BucketComponent {
+	return &s3BucketComponent{templatePath: templatePath}
+}
+
+func (comp *s3BucketComponent) WatchTypes() []runtime.Object {
+	return []runtime.Object{
+		&awsv1beta1.S3Bucket{},
+	}
+}
+
+func (_ *s3BucketComponent) IsReconcilable(_ *components.ComponentContext) bool {
+	// Secrets have no dependencies, always reconcile.
+	return true
+}
+
+func (comp *s3BucketComponent) Reconcile(ctx *components.ComponentContext) (components.Result, error) {
+	instance := ctx.Top.(*summonv1beta1.SummonPlatform)
+
+	extra := map[string]interface{}{}
+	extra["bucketName"] = instance.Spec.BucketName
+
+	var existing *awsv1beta1.S3Bucket
+	res, _, err := ctx.CreateOrUpdate(comp.templatePath, extra, func(goalObj, existingObj runtime.Object) error {
+		goal := goalObj.(*awsv1beta1.S3Bucket)
+		existing = existingObj.(*awsv1beta1.S3Bucket)
+		// Copy the Spec over.
+		existing.Spec = goal.Spec
+		return nil
+	})
+	return res, err
+}
